48551451b289dd5920acf0613daa3d98432fb9b9
sanmumuzi <332627946@qq.com>
2018-05-11 22:25:06 +0800

    Delete
    Turn to abandoned_qt branch

--- PixivSpider_GUI/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from Gui_init import GuiLogic
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    x = GuiLogic()
-    sys.exit(app.exec_())
